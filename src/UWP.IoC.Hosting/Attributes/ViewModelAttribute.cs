5557a3120d52ead87f53a32da752f512ce95190c
Rafael Yousuf <rafael.yousuf@outlook.com>
2019-11-16 16:51:57 +1100

    initial commit

---
+++ src/UWP.IoC.Hosting/Attributes/ViewModelAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UWP.IoC.Hosting.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class ViewModelAttribute : Attribute
+    {
+    }
+}
